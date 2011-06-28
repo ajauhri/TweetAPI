fd7079f6e8eaecc66266b113a852092e99d78231
Abhinav Jauhri <aj910martin@gmail.com>
2011-06-28 07:14:16 -0700

    setting modified

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
 __all__ = ['AWS_KEY' ,'AWS_SECRET', 'JSON_STREAMING_URI', 'SDB_DOMAIN', 'SDB_FILTER_DOMAIN', 'TWITTER_ID', 'TWITTER_PASSWORD']
 
-AWS_KEY = 'AKIAJ77EM7MALMT3WOPA'
-AWS_SECRET = '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'
+AWS_KEY = ''
+AWS_SECRET = ''
 JSON_STREAMING_URI = 'http://stream.twitter.com/1/statuses/filter.json'
 SDB_DOMAIN = 'db_tweets'
 SDB_FILTER_DOMAIN = 'db_search_keywords'
